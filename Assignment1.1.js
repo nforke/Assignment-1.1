7e50af0c0ca0e5cafcb5ef7afd5bb51aa62f57da
nforke <n.forke@gmail.com>
2019-11-28 17:05:48 -0600

    Create Assignment1.1.js

---
+++ Assignment1.1.js
@@ -0,0 +1,26 @@
+/*
+====================================
+; Title: Assignment 1.1
+; Author: Nicole Forke
+; Date: 28 November 2019
+; Modified By: Nicole Forke
+; Description: This is a simple JavaScript program using values and variables with two errors.
+
+The function is a simple math problem, the result should give the sum of x + y to z.
+====================================
+*/
+
+// Start program
+
+/*Expected Output:
+sum should be 30
+*/
+
+//variable declarations
+var x = 20;
+var y = "10";
+var z = x + y;
+
+consloe.log(z);
+
+//End program
